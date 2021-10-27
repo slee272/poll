bbdd43b87f7d03ab62a7b81c342554a90c8ce3a8
slee272 <slee272@naver.com>
2021-10-28 03:12:39 +0900

    Poll Service

---
+++ poll/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
+from django.urls import reverse
+from django.views import generic
+
+## Generic View (class-based views)
+class IndexView(generic.ListView):
+    template_name = 'poll/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'poll/detail.html'
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'poll/results.html'
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'poll/detail.html', {
+            'question':question,
+            'error_message':"You didn't select a choice."
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('poll:results', args=(question.id,)))
+
